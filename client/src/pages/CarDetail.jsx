e256872f3c60723f6e1d557200106babeb6e9fa2
sagark2808 <sagarkature1@gmail.com>
2025-08-12 11:07:14 +0530

    Add files via upload

---
+++ client/src/pages/CarDetail.jsx
@@ -0,0 +1,122 @@
+import React, { useEffect, useState } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
+import { assets, dummyCarData } from '../assets/assets';
+import Loading from '../components/Loading';
+import { useAppContext } from '../context/AppContext';
+import toast from 'react-hot-toast';
+
+const CarDetail = () => {
+
+    const { id } = useParams();
+    const navigate = useNavigate();
+    const [car, setCar] = useState(null);
+
+    const { currency, axios, cars, pickupDate, returnDate, setPickupDate, setReturnDate } = useAppContext()
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        try {
+            const { data } = await axios.post('/api/bookings/create', {
+                car: id,
+                pickupDate,
+                returnDate
+            })
+            if(data.success){
+                toast.success(data.message)
+                 navigate('/my-bookings')
+            }
+
+          
+        } catch (error) {
+            toast.error(error.message)
+        }
+    }
+
+    useEffect(() => {
+        setCar(cars.find(car => car._id === id))
+    }, [id, cars])
+
+    return car ? (
+        <div className='px-6 md:px-16 lg:px-24 xl:px-32 mt-16'>
+            <button className='flex items-center gap-2 mb-6 text-gray-500 cursor-pointer' onClick={() => navigate(-1)}>
+                <img src={assets.arrow_icon} alt="arrow_icon" className='rotate-180 opacity-65' /> Back to all cars
+            </button>
+
+            <div className='grid grid-cols-1 lg:grid-cols-3 gap-8 lg:gap-12'>
+                {/* Left: Car Image & Details */}
+                <div className='lg:col-span-2'>
+                    <img src={car.image} alt={car.brand} className='w-full h-auto md:max-h-100 object-cover rounded-xl mb-6 shadow-md' />
+
+                    <div className='space-y-6'>
+                        <div>
+                            <h1 className='text-3xl font-bold'>{car.brand} {car.model}</h1>
+                            <p className='text-gray-500 text-lg'>{car.category} • {car.year}</p>
+                        </div>
+
+                        <hr className='border-borderColor my-6' />
+
+                        <div className='grid grid-cols-2 sm:grid-cols-4 gap-4'>
+                            {[
+                                { icon: assets.users_icon, text: `${car.seating_capacity} Seats` },
+                                { icon: assets.fuel_icon, text: `${car.fuel_type}` },
+                                { icon: assets.car_icon, text: `${car.transmission}` },
+                                { icon: assets.location_icon, text: `${car.location}` },
+                            ].map(({ icon, text }) => (
+                                <div key={text} className='flex flex-col items-center bg-light p-4 rounded-lg'>
+                                    <img src={icon} alt={text} />
+                                    {text}
+                                </div>
+                            ))}
+                        </div>
+
+                        {/* Description */}
+                        <div>
+                            <h1 className='text-xl font-medium mb-3'>Description</h1>
+
+                            <p className='text-gray-500'>{car.description}</p>
+                        </div>
+
+                        {/* Features */}
+                        <div>
+                            <h1 className='text-xl font-medium mb-3'>Features</h1>
+
+                            <ul className='grid grid-cols-1 sm:grid-cols-2 gap-2'>
+                                {
+                                    ["360 Camera", "Bluetooth", "GPS", "Heated Seats", "Rear View Mirror"].map((item) => (
+                                        <li key={item} className='flex items-center text-gray-500'>
+                                            <img src={assets.check_icon} alt="check_icon" className='h-4 mr-2' />
+                                            {item}
+                                        </li>
+                                    ))
+                                }
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+                {/* Right: Booking Form */}
+                <form onSubmit={handleSubmit} className='shadow-lg h-max sticky top-18 rounded-xl p-6 space-y-6 text-gray-500'>
+                    <p className='flex items-center justify-between text-2xl text-gray-800 font-semibold'>{currency} {car.pricePerDay} <span className='text-base text-gray-400 font-normal'>per day</span></p>
+
+                    <hr className="border-borderColor my-6" />
+
+                    <div className='flex flex-col gap-2'>
+                        <label for="pickup-date">Pickup Date</label>
+                        <input onChange={e => setPickupDate(e.target.value)} value={pickupDate} className="border border-borderColor px-3 py-2 rounded-lg" required id="pickup-date" min={new Date().toISOString().split('T')[0]} type="date" />
+                    </div>
+
+                    <div className='flex flex-col gap-2'>
+                        <label for="return-date">Return Date</label>
+                        <input onChange={e => setReturnDate(e.target.value)} value={returnDate} className="border border-borderColor px-3 py-2 rounded-lg" required id="return-date" min={new Date().toISOString().split('T')[0]} type="date" />
+                    </div>
+
+                    <button type='submit' className="w-full bg-primary hover:bg-primary-dull transition-all py-3 font-medium text-white rounded-xl cursor-pointer">Book Now</button>
+
+                    <p className='text-center text-sm'>No credit card required to reserve</p>
+                </form>
+            </div>
+        </div>
+    ) : <Loading />
+}
+
+export default CarDetail
